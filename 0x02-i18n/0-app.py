13bb264f6d745805029057494cd0bbefef623bc8
BnKeTia <bernardnketia51@gmail.com>
2024-05-29 01:08:46 +0000

    basic babel set up

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+"""A Module containing Flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/')
+def root():
+    """A Function that defines the route to an html template"""
+
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
