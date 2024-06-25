3acbf39bea1b66660a1584cab7241ce06fcd5015
BnKeTia <bernardnketia51@gmail.com>
2024-06-25 18:41:05 +0000

    created the job processor

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+// Import the Redis client
+const { createClient } = require('redis');
+
+// Create a Redis client
+const client = createClient();
+
+// Handle connection events
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Function to publish messages
+function publishMessage(message, time) {
+  setTimeout(() => {
+    console.log(`About to send ${message}`);
+    client.publish('holberton school channel', message);
+  }, time);
+}
+
+// Call the publishMessage function with specified messages and time intervals
+publishMessage('Holberton Student #1 starts course', 100);
+publishMessage('Holberton Student #2 starts course', 200);
+publishMessage('KILL_SERVER', 300);
+publishMessage('Holberton Student #3 starts course', 400);
